a87819f57e68740eb3a97b7f46eed26f961975cc
Debondt Didier <p@henix.be>
2014-11-05 11:10:03 +0100

    Fonction d'ajout de mailqueue

---
+++ inc/mailqueue.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+/**
+ * Fonction de création de queue d'email
+ *
+ * @param mixed $sujet
+ * @param mixed $html
+ * @param mixed $texte
+ * @access public
+ */
+function inc_mailqueue_dist($sujet, $html=null, $texte=null) {
+
+    // On bloque en cas de nullité total du message
+    if (empty($html) and empty($texte))
+        return false;
+
+    // A prioris on a pas besoin de gérer plus de chose, facteur ce dérbouillera avec html et/ou texte
+    $id_mailqueue = objet_inserer(
+        'mailqueue',
+        null,
+        array(
+            'sujet' => $sujet,
+            'html' => $html,
+            'texte' => $texte,
+            'etat' => 'attente'
+        )
+    );
+
+
+
+    return $id_mailqueue;
+}
